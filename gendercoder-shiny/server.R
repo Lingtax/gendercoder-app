8761cb8a3e9feebebe7514db96e68e7aff449006
Lingtax <m.ling@deakin.edu.au>
2021-03-25 10:14:31 +1100

    Add upload features

--- gendercoder-shiny/server.R
+++ gendercoder-shiny/server.R
@@ -1,10 +1,20 @@
 library(shiny)
 library(haven)
 library(gendercoder)
+library(dplyr)
 
 
 shinyServer(function(input, output) {
 
+    output$out1 <-  renderTable({
+        req(input$file1)
+        
+        if (grepl(".sav$", input$file1$name)) df <- read_sav(input$file1$datapath) 
+        if (grepl(".dta$", input$file1$name)) df <- read_dta(input$file1$datapath) 
+        if (grepl(".csv$", input$file1$name)) df <- read_csv(input$file1$datapath) 
+       
+        head((df))
+        })
+    
    
-
 })
--- gendercoder-shiny/ui.R
+++ gendercoder-shiny/ui.R
@@ -2,7 +2,17 @@ library(shiny)
 library(shinydashboard)
 
 dashboardPage(
-    dashboardHeader(),
-    dashboardSidebar(),
-    dashboardBody()
+    dashboardHeader(title = "Gendercoder app"),
+    dashboardSidebar(
+        fileInput("file1", "Choose data File",
+                  multiple = FALSE,
+                  accept = c("text/csv",
+                             "text/comma-separated-values,text/plain",
+                             ".csv", 
+                             ".dta", 
+                             ".sav"))
+    ),
+    dashboardBody(
+        tableOutput("out1")
+    )
 )
